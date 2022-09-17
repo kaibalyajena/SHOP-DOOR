553fdf5dd7efa9c6e9916e2b60e852d18fdc5858
kaibalya jena <kaibalya18j@gmail.com>
2022-09-17 23:39:35 +0530

    file structures and themes

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthSreen extends StatefulWidget {
+  const AuthSreen({super.key});
+
+  @override
+  State<AuthSreen> createState() => _AuthSreenState();
+}
+
+class _AuthSreenState extends State<AuthSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
