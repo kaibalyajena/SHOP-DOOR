6d08516a0a525c1bf04cf6380d3f8c47ab203fa7
kaibalya jena <kaibalya18j@gmail.com>
2022-09-18 23:39:44 +0530

    var

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class GlobalVariables {
   static const appBarGradient = LinearGradient(
     colors: [
-      Color.fromARGB(255, 29, 201, 192),
+      Color.fromARGB(255, 29, 201, 191),
       Color.fromARGB(255, 125, 221, 216),
     ],
     stops: [0.5, 1.0],
